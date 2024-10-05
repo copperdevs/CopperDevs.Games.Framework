b63709880e9dcd4bc67eafdbf70e7825ed195e7c
copperdevs <79734708+copperdevs@users.noreply.github.com>
2024-10-05 01:00:26 -0500

    idc about naming

--- src/CopperDevs.Games.Framework/Game.Systems.cs
+++ src/CopperDevs.Games.Framework/Game.Systems.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using CopperDevs.Games.Framework.ECS;
 using fennecs;
 
@@ -85,21 +86,25 @@ public partial class Game
         where TStreamType : StreamType, new() =>
         SpawnSystemEntity<TSystemType, TStreamType>(new TSystem(), filters);
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public QueryBuilder<C1> QueryEntities<C1>()
         where C1 : notnull =>
         ecsWorld.Query<C1>();
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public QueryBuilder<C1, C2> QueryEntities<C1, C2>()
         where C1 : notnull
         where C2 : notnull =>
         ecsWorld.Query<C1, C2>();
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public QueryBuilder<C1, C2, C3> QueryEntities<C1, C2, C3>()
         where C1 : notnull
         where C2 : notnull
         where C3 : notnull =>
         ecsWorld.Query<C1, C2, C3>();
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public QueryBuilder<C1, C2, C3, C4> QueryEntities<C1, C2, C3, C4>()
         where C1 : notnull
         where C2 : notnull
@@ -107,6 +112,7 @@ public partial class Game
         where C4 : notnull =>
         ecsWorld.Query<C1, C2, C3, C4>();
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public QueryBuilder<C1, C2, C3, C4, C5> QueryEntities<C1, C2, C3, C4, C5>()
         where C1 : notnull
         where C2 : notnull
@@ -115,21 +121,25 @@ public partial class Game
         where C5 : notnull =>
         ecsWorld.Query<C1, C2, C3, C4, C5>();
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public QueryBuilder<C1> QueryEntities<C1>(params IFilter[] filters)
         where C1 : notnull =>
         filters.Aggregate(QueryEntities<C1>(), (current, filter) => filter.FilterQuery(current));
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public QueryBuilder<C1, C2> QueryEntities<C1, C2>(params IFilter[] filters)
         where C1 : notnull
         where C2 : notnull =>
         filters.Aggregate(ecsWorld.Query<C1, C2>(), (current, filter) => filter.FilterQuery(current));
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public QueryBuilder<C1, C2, C3> QueryEntities<C1, C2, C3>(params IFilter[] filters)
         where C1 : notnull
         where C2 : notnull
         where C3 : notnull =>
         filters.Aggregate(ecsWorld.Query<C1, C2, C3>(), (current, filter) => filter.FilterQuery(current));
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public QueryBuilder<C1, C2, C3, C4> QueryEntities<C1, C2, C3, C4>(params IFilter[] filters)
         where C1 : notnull
         where C2 : notnull
@@ -137,6 +147,7 @@ public partial class Game
         where C4 : notnull =>
         filters.Aggregate(ecsWorld.Query<C1, C2, C3, C4>(), (current, filter) => filter.FilterQuery(current));
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public QueryBuilder<C1, C2, C3, C4, C5> QueryEntities<C1, C2, C3, C4, C5>(params IFilter[] filters)
         where C1 : notnull
         where C2 : notnull
